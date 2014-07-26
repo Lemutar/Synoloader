65a6ca9e5e054a75fb04f336b5ff030b0dd3e334
Lemutar <lemutar@gmail.com>
2014-07-26 10:10:19 +0200

    create firefox profile dinamicly

--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 # pull the svn files
-cd src
 
-rm ../firefox_profile/extensions/lemutar@gmail.com/* -rf
-cp ./* ../firefox_profile/extensions/lemutar@gmail.com/ -rf
+
+rm ./firefox_profile/  -rf
+firefox -CreateProfile "Synoloader_Test $PWD/firefox_profile" -no-remote
+mkdir ./firefox_profile/extensions
+mkdir ./firefox_profile/extensions/lemutar@gmail.com
+cp ./src/* ./firefox_profile/extensions/lemutar@gmail.com/ -rf
+cp ./firefox_profile_config/prefs.js ./firefox_profile/prefs.js
 
 cd ..
 echo "TESTING"
-ruby fire-test-runner --profile="firefox_profile"  ./test/ 
+#ruby fire-test-runner --port=9999 --wait=20 --profile="firefox_profile"  ./test/ 
 
