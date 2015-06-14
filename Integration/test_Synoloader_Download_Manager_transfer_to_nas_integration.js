52eaef661332e78cec3ec913875b580d43d8e325
lemutar <lemutar@gmail.com>
2015-06-14 18:39:33 +0200

    fix test

--- Integration/test_Synoloader_Download_Manager_transfer_to_nas_integration.js
+++ Integration/test_Synoloader_Download_Manager_transfer_to_nas_integration.js
@@ -5,12 +5,15 @@ utils.include( utils.baseURL + "../src/modules/DownloadManager.js");
 
 function setUp()
 {
- SynoLoader_DownloadManager.password = "1234";
- SynoLoader_DownloadManager.username = "synoloader_tester";
- SynoLoader_DownloadManager.url_to_connect = 'http://z35.no-ip.org:5050';
- SynoLoader_DownloadManager.set_protocol();
- SynoLoader_DownloadManager.Util.show_log=true;
- 
+    SynoLoader_DownloadManager.password = "1234";
+    SynoLoader_DownloadManager.username = "synoloader_tester";
+    SynoLoader_DownloadManager.url_to_connect = 'http://z35.no-ip.org:5050';
+    var preferences = Components.classes["@mozilla.org/preferences-service;1"]
+        .getService(Components.interfaces.nsIPrefService)
+        .getBranch("extensions.SynoLoader.");
+    preferences.setCharPref('DSM_Verison',"2");
+    SynoLoader_DownloadManager.set_protocol();
+    SynoLoader_DownloadManager.Util.show_log=true;
 }
 
 function tearDown() {
@@ -30,14 +33,9 @@ function shutDown()
 
 }
 
-
-
 test_Protocol_DownloadManager_transfer_to_nas_torrent_file.description = 'test_Protocol_DownloadManager_transfer_to_nas_torrent_file';
 test_Protocol_DownloadManager_transfer_to_nas_torrent_file.priority    = 'must';
 function test_Protocol_DownloadManager_transfer_to_nas_torrent_file() {
-
-
-
  var NotificationMock = new Mock(Notification);
  var loaded = { value : false };
  NotificationMock.expect('show', ["Send torrent file to NAS","http://releases.ubuntu.com/14.04/ubuntu-14.04-desktop-amd64.iso.torrent"], null)
@@ -49,13 +47,9 @@ function test_Protocol_DownloadManager_transfer_to_nas_torrent_file() {
 
 }
 
-
 test_Protocol_DownloadManager_transfer_to_nas_link.description = 'test_Protocol_DownloadManager_transfer_to_nas_link';
 test_Protocol_DownloadManager_transfer_to_nas_link.priority    = 'must';
 function test_Protocol_DownloadManager_transfer_to_nas_link() {
-
-
-
  var NotificationMock = new Mock(Notification);
  var loaded = { value : false };
  NotificationMock.expect('show', ["Send link","http://releases.ubuntu.com/14.04/ubuntu-14.04-desktop-amd64.iso"])
@@ -63,16 +57,16 @@ function test_Protocol_DownloadManager_transfer_to_nas_link() {
  SynoLoader_DownloadManager.Notification = NotificationMock;
  SynoLoader_DownloadManager.transfer_to_nas("http://releases.ubuntu.com/14.04/ubuntu-14.04-desktop-amd64.iso");
  utils.wait(loaded);
-
-
 }
 
 test_Protocol_DownloadManager_transfer_to_nas_torrent_file_old.description = 'test_Protocol_DownloadManager_transfer_to_nas_torrent_file_old';
-test_Protocol_DownloadManager_transfer_to_nas_torrent_file_old.priority    = 'must';
+test_Protocol_DownloadManager_transfer_to_nas_torrent_file_old.priority    = 'never';
 function test_Protocol_DownloadManager_transfer_to_nas_torrent_file_old() {
-
-
- SynoLoader_DownloadManager.set_protocol("1");
+ var preferences = Components.classes["@mozilla.org/preferences-service;1"]
+     .getService(Components.interfaces.nsIPrefService)
+     .getBranch("extensions.SynoLoader.");
+ preferences.setCharPref('DSM_Verison',"1");
+ SynoLoader_DownloadManager.set_protocol();
  var NotificationMock = new Mock(Notification);
  var loaded = { value : false };
  NotificationMock.expect('show', ["Send link","http://releases.ubuntu.com/14.04/ubuntu-14.04-desktop-amd64.iso.torrent"], null)
@@ -80,7 +74,6 @@ function test_Protocol_DownloadManager_transfer_to_nas_torrent_file_old() {
  SynoLoader_DownloadManager.Notification = NotificationMock;
  SynoLoader_DownloadManager.transfer_to_nas("http://releases.ubuntu.com/14.04/ubuntu-14.04-desktop-amd64.iso.torrent");
  utils.wait(loaded);
-
 }
 
 
