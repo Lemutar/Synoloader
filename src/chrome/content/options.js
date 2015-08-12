046cafe356453b2ad38e78259d69b6a4530c956b
Armando Lüscher <armando@noplanman.ch>
2015-08-12 05:34:31 +0200

    Clean up options.js

--- src/chrome/content/options.js
+++ src/chrome/content/options.js
@@ -1,80 +1,86 @@
-var SynoLoader = {};
-Components.utils.import("resource://SynoLoader/DownloadManager.js", SynoLoader);
+const { classes: Cc, interfaces: Ci } = Components;
 
-SynoLoader.SetUserNamePassword = function() {
-    this.myLoginManager = Components.classes["@mozilla.org/login-manager;1"]
-        .getService(Components.interfaces.nsILoginManager);
-    // create instance of LoginInfo
-    this.nsLoginInfo = new Components.Constructor("@mozilla.org/login-manager/loginInfo;1",
-        Components.interfaces.nsILoginInfo,
-        "init");
-    // ask for credentials
-    this.prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-        .getService(Components.interfaces.nsIPromptService);
+if (typeof SynoLoader === 'undefined') {
+    var SynoLoader = {};
 
-    var username = {
-        value: ""
-    };
-    var password = {
-        value: ""
-    };
-    var check = {
-        value: false
-    };
+    Components.utils.import('resource://SynoLoader/DownloadManager.js', SynoLoader);
 
-    var logins = this.myLoginManager.findLogins({}, 'chrome://SynoLoader.Pass', null, 'User Registration');
-    for (var index = 0; index < logins.length; index++) {
-        username = {
-            value: logins[index].username
+    (function () {
+        this.SetUserNamePassword = function () {
+            this.myLoginManager = Cc['@mozilla.org/login-manager;1']
+                .getService(Ci.nsILoginManager);
+            // create instance of LoginInfo
+            this.nsLoginInfo = new Components.Constructor(
+                '@mozilla.org/login-manager/loginInfo;1',
+                Ci.nsILoginInfo,
+                'init'
+            );
+            // ask for credentials
+            this.prompts = Cc['@mozilla.org/embedcomp/prompt-service;1']
+                .getService(Ci.nsIPromptService);
+
+            var username = {
+                value: ''
+            };
+            var password = {
+                value: ''
+            };
+            var check = {
+                value: false
+            };
+
+            var logins = this.myLoginManager.findLogins({}, 'chrome://SynoLoader.Pass', null, 'User Registration');
+            for (var index = 0; index < logins.length; index++) {
+                username = {
+                    value: logins[index].username
+                };
+                password = {
+                    value: logins[index].password
+                };
+                this.myLoginManager.removeLogin(logins[index]);
+            }
+
+            this.prompts.promptUsernameAndPassword(window, '', 'Please enter your login for your SynoNas', username, password, null, {});
+            this.myLoginInfo = new this.nsLoginInfo('chrome://SynoLoader.Pass', null, 'User Registration', username.value, password.value, '', '');
+            this.myLoginManager.addLogin(this.myLoginInfo);
+            this.SynoLoader_DownloadManager.username = username.value;
+            this.SynoLoader_DownloadManager.password = password.value;
         };
-        password = {
-            value: logins[index].password
+
+        this.checkConnection = function () {
+            if (this.SynoLoader_DownloadManager.connecting === false) {
+                this.SynoLoader_DownloadManager.connect_to_nas();
+                this.UpdateStatus();
+            }
         };
-        this.myLoginManager.removeLogin(logins[index]);
-    }
 
-    this.prompts.promptUsernameAndPassword(window, '', 'Please enter your login for your SynoNas', username, password, null, {});
-    this.myLoginInfo = new this.nsLoginInfo('chrome://SynoLoader.Pass',
-        null, 'User Registration',
-        username.value, password.value, "", "");
-    this.myLoginManager.addLogin(this.myLoginInfo);
-    this.SynoLoader_DownloadManager.username = username.value;
-    this.SynoLoader_DownloadManager.password = password.value;
+        this.option_init = function () {
+            this.was_connecting = false;
+            if (this.SynoLoader_DownloadManager.is_connect === true) {
+                document.getElementById('SynoLoader_check_connection_image').setAttribute('style', 'list-style-image: url(chrome://SynoLoader/skin/approval.png)');
+            }
+            this.UpdateStatus();
+            this.interval = window.setInterval(function () {
+                SynoLoader.UpdateStatus();
+            }, 500);
+        };
 
-};
+        this.UpdateStatus = function () {
+            if (this.SynoLoader_DownloadManager.connecting === true) {
+                this.was_connecting = true;
+                document.getElementById('SynoLoader_check_connection_image').setAttribute('style', 'list-style-image: url(chrome://SynoLoader/skin/hour_glass.png)');
+            } else if (this.was_connecting === true) {
+                if (this.SynoLoader_DownloadManager.is_connect === true) {
+                    document.getElementById('SynoLoader_check_connection_image').setAttribute('style', 'list-style-image: url(chrome://SynoLoader/skin/approval.png)');
+                } else {
+                    document.getElementById('SynoLoader_check_connection_image').setAttribute('style', 'list-style-image: url(chrome://SynoLoader/skin/cancel.png)');
+                }
+            }
+        };
+    }).apply(SynoLoader);
+}
 
-SynoLoader.checkConnection = function() {
-    if (this.SynoLoader_DownloadManager.connecting === false) {
-        this.SynoLoader_DownloadManager.connect_to_nas();
-        this.UpdateStatus();
-    }
-};
-
-SynoLoader.option_init = function() {
-    SynoLoader.was_connecting = false;
-    if (true === this.SynoLoader_DownloadManager.is_connect) {
-        document.getElementById("SynoLoader_check_connection_image").setAttribute('style', "list-style-image: url('chrome://SynoLoader/skin/approval.png')");
-    }
-    this.UpdateStatus();
-    this.interval = window.setInterval(function() {
-        SynoLoader.UpdateStatus();
-    }, 500);
-};
-
-SynoLoader.UpdateStatus = function() {
-    if (this.SynoLoader_DownloadManager.connecting === true) {
-        SynoLoader.was_connecting = true;
-        document.getElementById("SynoLoader_check_connection_image").setAttribute('style', "list-style-image: url('chrome://SynoLoader/skin/hour_glass.png')");
-    } else if (SynoLoader.was_connecting === true) {
-        if (true === this.SynoLoader_DownloadManager.is_connect) {
-            document.getElementById("SynoLoader_check_connection_image").setAttribute('style', "list-style-image: url('chrome://SynoLoader/skin/approval.png')");
-        } else {
-            document.getElementById("SynoLoader_check_connection_image").setAttribute('style', "list-style-image: url('chrome://SynoLoader/skin/cancel.png')");
-        }
-    }
-};
-
-window.addEventListener("load", function load(e) {
-    window.removeEventListener("load", load, false); //remove listener, no longer needed
+window.addEventListener('load', function load (e) {
+    window.removeEventListener('load', load, false); //remove listener, no longer needed
     SynoLoader.option_init();
 }, false);
