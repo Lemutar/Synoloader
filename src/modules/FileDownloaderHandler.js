fd9ed75abc22b7a8aa31c1549aefbb1defce4274
Armando Lüscher <armando@noplanman.ch>
2015-08-12 03:53:10 +0200

    Clean up FileDownloaderHandler.js

--- src/modules/FileDownloaderHandler.js
+++ src/modules/FileDownloaderHandler.js
@@ -1,26 +1,18 @@
-var EXPORTED_SYMBOLS = ["FileDownloaderHandler"];
+var EXPORTED_SYMBOLS = ['FileDownloaderHandler'];
+const { classes: Cc, interfaces: Ci } = Components;
 
-
-
-
-if (typeof FileDownloaderHandler == "undefined") {
+if (typeof FileDownloaderHandler === 'undefined') {
     var FileDownloaderHandler = {};
 
+    Components.utils.import('resource://gre/modules/Downloads.jsm');
+    Components.utils.import('resource://gre/modules/Task.jsm');
 
-    Components.utils.import("resource://gre/modules/Downloads.jsm");
-    Components.utils.import("resource://gre/modules/Task.jsm");
-
-    (function() {
-
-
-        this.get_file_content = function(source, destination, succeded) {
-            Task.spawn(function() {
+    (function () {
+        this.get_file_content = function (source, destination, succeeded) {
+            Task.spawn(function () {
                 /*jshint moz: true */
                 yield Downloads.fetch(source, destination);
-            }).then(succeded, Components.utils.reportError);
+            }).then(succeeded, Components.utils.reportError);
         };
-
     }).apply(FileDownloaderHandler);
-
-
 }
