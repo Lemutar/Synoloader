61f540384a035c07707e848557989070adb4fefc
Armando Lüscher <armando@noplanman.ch>
2015-08-12 02:24:01 +0200

    Clean up Notification.js

--- src/modules/Notification.js
+++ src/modules/Notification.js
@@ -1,26 +1,26 @@
-var EXPORTED_SYMBOLS = ["Notification"];
+var EXPORTED_SYMBOLS = ['Notification'];
+const { classes: Cc, interfaces: Ci } = Components;
 
-var ww = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
-    .getService(Components.interfaces.nsIWindowWatcher);
+if (typeof Notification === 'undefined') {
+    var Notification = {};
 
-
-Notification = function() {
-
-};
-try {
-    Notification.AlertInterface = Components.classes['@mozilla.org/alerts-service;1'].getService(Components.interfaces.nsIAlertsService);
-
-} catch (e) {
-    // prevents runtime error on platforms that don't implement nsIAlertsService
-}
-
-Notification.show_not = false;
-Notification.show = function(title, text) {
-    if (this.show_not) {
+    (function () {
         try {
-            this.AlertInterface.showAlertNotification("chrome://SynoLoader/skin/Syno.png", title, text, false, '', null);
+            Notification.AlertInterface = Cc['@mozilla.org/alerts-service;1']
+                .getService(Ci.nsIAlertsService);
         } catch (e) {
             // prevents runtime error on platforms that don't implement nsIAlertsService
         }
-    }
-};
+
+        this.show_not = false;
+        this.show = function (title, text) {
+            if (Notification.show_not) {
+                try {
+                    Notification.AlertInterface.showAlertNotification('chrome://SynoLoader/skin/Syno.png', title, text, false, '', null);
+                } catch (e) {
+                    // prevents runtime error on platforms that don't implement nsIAlertsService
+                }
+            }
+        };
+    }).apply(Notification);
+}
