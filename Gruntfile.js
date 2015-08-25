79aab63880e4750ecd5deb1b042f35c9ff4db10f
Armando Lüscher <armando@noplanman.ch>
2015-08-25 07:59:12 +0200

    Beautify integration tests with grunt too.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,16 @@
 module.exports = function(grunt) {
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js', 'Integration/**/*.js'],
             options: {
                 jshintrc: '.jshintrc' // relative to Gruntfile
                     // options here to override JSHint defaults
             }
         },
         jsbeautifier: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js', 'Integration/**/*.js']
         }
     });
     grunt.loadNpmTasks('grunt-contrib-jshint');
--- Integration/DownloadManager_transfer_to_nas_integration.js
+++ Integration/DownloadManager_transfer_to_nas_integration.js
@@ -37,24 +37,8 @@ function setUpNotificationMock(title, text) {
 }
 
 
-test_DownloadManager_loadDownloadList.description = "test_DownloadManager_loadDownloadList";
-test_DownloadManager_loadDownloadList.priority    = "low";
-function test_DownloadManager_loadDownloadList() {
-    let downloadItems = {};
-    DownloadManager.loadDownloadList((items) => {
-            downloadItems = items;
-            loaded.value = true;
-        }, (items) => {
-            downloadItems.length = 99;
-            loaded.value = true;
-        }
-    );
-
-    utils.wait(loaded);
-    assert.equals(downloadItems.length, 2);
-}
-
 test_Protocol_DownloadManager_transferToNas_torrent_file.description = "test_Protocol_DownloadManager_transferToNas_torrent_file";
+
 function test_Protocol_DownloadManager_transferToNas_torrent_file() {
     setUpNotificationMock("Send torrent file to NAS", "http://releases.ubuntu.com/14.04/ubuntu-14.04-desktop-amd64.iso.torrent");
     DownloadManager.transferToNas("http://releases.ubuntu.com/14.04/ubuntu-14.04-desktop-amd64.iso.torrent");
@@ -62,8 +46,26 @@ function test_Protocol_DownloadManager_transferToNas_torrent_file() {
 }
 
 test_Protocol_DownloadManager_transferToNas_link.description = "test_Protocol_DownloadManager_transferToNas_link";
+
 function test_Protocol_DownloadManager_transferToNas_link() {
     setUpNotificationMock("Send link", "http://releases.ubuntu.com/14.04/ubuntu-14.04-desktop-amd64.iso");
     DownloadManager.transferToNas("http://releases.ubuntu.com/14.04/ubuntu-14.04-desktop-amd64.iso");
     utils.wait(loaded);
+}
+
+test_DownloadManager_loadDownloadList.description = "test_DownloadManager_loadDownloadList";
+test_DownloadManager_loadDownloadList.priority = "low";
+
+function test_DownloadManager_loadDownloadList() {
+    let downloadItems = {};
+    DownloadManager.loadDownloadList((items) => {
+        downloadItems = items;
+        loaded.value = true;
+    }, (items) => {
+        downloadItems.length = 99;
+        loaded.value = true;
+    });
+
+    utils.wait(loaded);
+    assert.equals(2, downloadItems.length);
 }
