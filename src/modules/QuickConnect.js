2683f10fdff31e9fdd2a94435926dd0e1f8e5b99
lemutar <lemutar@gmail.com>
2016-02-27 14:53:07 +0100

    fix for quick connetc

--- src/modules/QuickConnect.js
+++ src/modules/QuickConnect.js
@@ -1,12 +1,67 @@
 var EXPORTED_SYMBOLS = ["QuickConnect"];
 
 Components.utils.import("resource://SynoLoader/Request.js");
+Components.utils.import("resource://SynoLoader/Util.js");
 
 if (typeof QuickConnect === "undefined") {
     var QuickConnect = function(timeoutRelay, timeoutInternal, protocol, port) {
-        this.relayServer = "https://ukc.synology.com/Serv.php";
+        this.relayServer = "https://global.quickconnect.to/Serv.php";
 
-        this.getServerInfo = (quickConnectID, callback) => {
+        this.getQuickConnectServers = (callback) => {
+            let response = {
+                success: false,
+                quick_connect_servers: [],
+            };
+
+            new Request(
+                this.relayServer,
+                JSON.stringify({
+                    version: "1",
+                    command: "get_site_list",
+                }),
+                timeoutRelay, (relayResponse) => {
+                    if (relayResponse.status === 200) {
+                        if (relayResponse.json.errno === 0) {
+                            response.success = true;
+                            relayResponse.json.sites.forEach((entry) => {
+                                response.quick_connect_servers.push(entry);
+                            });
+                            Util.log(relayResponse.text);
+                        }
+                    }
+                    callback(response);
+                }).post();
+        };
+
+        this.getServerTunnelInfo = (serverUrl, quickConnectID, callback) => {
+            let response = {
+                success: false,
+                ip: "",
+                port: ""
+            };
+
+            new Request(
+                "https://" + serverUrl + "/Serv.php",
+                JSON.stringify({
+                    version: "1",
+                    command: "request_tunnel",
+                    serverID: quickConnectID,
+                    id: "dsm_https"
+                }),
+                timeoutRelay, (relayResponse) => {
+                    if (relayResponse.status === 200) {
+                        if (relayResponse.json.errno === 0) {
+                            response.ip = self._settings.quickConnectId + "." + serverUrl.split(".").slice(0, 2) + ".quickconnect.to";
+                            response.port = data.service.https_port ? data.service.https_port : 443;
+                            response.success = true;
+                        }
+                        Util.log(relayResponse.text);
+                    }
+                    callback(response);
+                }).post();
+        };
+
+        this.getServerInfo = (serverUrl, quickConnectID, callback) => {
             let response = {
                 success: false,
                 internalIP: [],
@@ -14,7 +69,7 @@ if (typeof QuickConnect === "undefined") {
             };
 
             new Request(
-                this.relayServer,
+                "https://" + serverUrl + "/Serv.php",
                 JSON.stringify({
                     version: "1",
                     command: "get_server_info",
@@ -30,12 +85,36 @@ if (typeof QuickConnect === "undefined") {
                             });
                             response.externalIP = relayResponse.json.server.external.ip;
                         }
+                        Util.log(relayResponse.text);
                     }
                     callback(response);
                 }
             ).post();
         };
 
+        this.getConnections = (quickConnectID, callback) => {
+            let first = true;
+            let errorCount = 0;
+            this.getQuickConnectServers((responseServers) => {
+                if (responseServers.success === true) {
+                    responseServers.quick_connect_servers.forEach((entry) => {
+                        this.getServerInfo(entry, quickConnectID, (responseServerInfo) => {
+                            if (first && responseServerInfo.success) {
+                                callback(responseServerInfo);
+                            } else {
+                                errorCount++;
+                                if (errorCount >= responseServers.quick_connect_servers.length) {
+                                    callback(responseServerInfo);
+                                }
+                            }
+                        });
+                    });
+                } else {
+                    callback(responseServers);
+                }
+            });
+
+        };
 
         this.checkInternalIPs = (internalIPs, callback) => {
             let first = true,
@@ -68,14 +147,13 @@ if (typeof QuickConnect === "undefined") {
             });
         };
 
-
         this.get = (quickConnectID, callback) => {
             let response = {
                 success: false,
                 ip: ""
             };
 
-            this.getServerInfo(quickConnectID, (serverInfoResponse) => {
+            this.getConnections(quickConnectID, (serverInfoResponse) => {
                 if (serverInfoResponse.success) {
                     this.checkInternalIPs(serverInfoResponse.internalIP, (response_ip) => {
                         response.success = true;
